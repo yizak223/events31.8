f9ffa76fe71b124e48c2f79990107e2f7632b6dc
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-08-31 22:49:19 +0300

    commit

---
+++ 7-9/main.js
@@ -0,0 +1,13 @@
+//8
+var age=prompt('age')
+if(age>18){
+    myDiv.innerHTML+=`
+    <p>wellcome</p>`
+    myDiv.style=`color:red`
+}
+else{
+    myDiv.innerHTML+=`
+    <p>wellcome</p>`
+    myDiv.style=`color:blue`
+}
+//9
