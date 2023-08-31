2470b881912a822e2a2e5188bcd45744707f2c1d
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-08-31 23:40:11 +0300

    commit

--- 7-9/main.js
+++ 7-9/main.js
@@ -1,13 +1,71 @@
 //8
-var age=prompt('age')
-if(age>18){
-    myDiv.innerHTML+=`
-    <p>wellcome</p>`
-    myDiv.style=`color:red`
-}
-else{
-    myDiv.innerHTML+=`
-    <p>wellcome</p>`
-    myDiv.style=`color:blue`
-}
-//9
+// function changeToGreen() {
+//     myDiv.style=`color:green`
+// }
+// var age=prompt('age')
+// if(age>18){
+//     myDiv.innerHTML+=`
+//     <p onmouseover="changeToGreen()">wellcome</p>`
+//     myDiv.style=`color:red`
+// }
+// else{
+//     myDiv.innerHTML+=`
+//     <p onmouseover="changeToGreen()">wellcome</p>`
+//     myDiv.style=`color:blue`
+// }
+//9    
+// function printNum() {
+//     pargMy.innerHTML+=`${arrayGuess}`
+// }
+// var arrayGuess=[]
+// for (let i = 0; i < 5; i++) {
+//     arrayGuess[i]= prompt('num')
+// }
+
+// var computerRandom=Math.floor(Math.random()*56)
+// for (let i = 0; i < 5; i++) {
+//     if(arrayGuess[i]==computerRandom){
+//         pargMy.innerHTML+=`goood!`
+//     }
+//     else{
+//         pargMy.innerHTML+=`bad!`
+//     }
+// }
+//10
+// function printNum() {
+//     pargMy.innerHTML += `${arrayGuess}`
+// }
+// var arrayGuess = []
+// for (let i = 0; i < 5; i++) {
+//     arrayGuess[i] = prompt('num')
+// }
+// var computerRandomArray = []
+// for (let i = 0; i < 5; i++) {
+//     computerRandomArray[i] = Math.floor(Math.random()*56)
+//     forComputerGurss.innerHTML += `
+//     <p>${computerRandomArray[i]}</p>
+//     `
+// }
+// var t = 0
+// for (let i = 0; i < arrayGuess.length; i++) {
+//     for (let j = 0; j < computerRandomArray.length; j++) {
+//         if (arrayGuess[0] == computerRandomArray[0]) {
+//             forAlertSuccess.innerHTML += `<p>goood!</p>`
+//             t++
+//         }
+//         if (t > 0) {
+//             break;
+//         }
+//     }
+//     if (t > 0) {
+//         break;
+//     }
+// }
+// if(t==0){
+//     forAlertSuccess.innerHTML += `<p>bad!</p>`
+// }
+//11
+
+
+
+
