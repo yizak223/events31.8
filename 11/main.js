ab47838402b1b0409773f5a55a1aab7b4e6b066f
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-09-03 00:28:19 +0300

    commit

---
+++ 11/main.js
@@ -0,0 +1,14 @@
+
+
+
+console.log(new Date().getFullYear());
+
+
+
+function printToLogNamE() {
+    console.log(forName.value);
+}
+
+function printToLogAge(){
+    console.log(forAge.value.getFullYear());
+}
