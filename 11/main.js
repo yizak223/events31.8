2005271d6480f36adeabb8de033c82564f528c5b
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-09-03 01:14:07 +0300

    commit

--- 11/main.js
+++ 11/main.js
@@ -1,14 +1,41 @@
 
 
 
-console.log(new Date().getFullYear());
 
 
-
-function printToLogNamE() {
-    console.log(forName.value);
+// function printToLogNamE() {
+//     console.log(forName.value);
+// }
+function refreshPage() {
+    location.reload()
 }
 
-function printToLogAge(){
-    console.log(forAge.value.getFullYear());
+function printToLogAge() {
+    // var yearInputUser=
+    var yearToday = new Date().getFullYear()
+    console.log(yearToday);
+    var dateInput = forAge.value
+    console.log(dateInput);
+    var makeItDate = new Date(dateInput);
+    var year = makeItDate.getFullYear();
+    console.log(year);
+    console.log(yearToday - year);
+    if (yearToday - year >= 18) {
+        myDiv.innerHTML = `
+        <h1  onmouseover="changeToGreen()" >wellcome</h1>`
+        myDiv.style = `color:blue;`
+    }
+    else {
+        myDiv.innerHTML = `
+        <p>no entry</p>
+        <img src='https://phabcart.imgix.net/cdn/scdn/images/uploads/636844577374966611_7a003bk_web_600.jpg'>`
+        myDiv.style = `color:red`
+        setTimeout(refreshPage, 3000);
+    }
 }
+function changeToGreen() {
+    myDiv.style=`color:green`
+}
+
+
+
