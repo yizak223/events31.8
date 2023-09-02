2b55bf2c18baff12a6bf82d1ab629ce77340bd4c
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-09-02 20:40:12 +0300

    task10

--- 9-11/main.js
+++ 9-11/main.js
@@ -24,3 +24,36 @@ function showIfUserSiccess() {
     myDiv.innerHTML+=`
     <p>bad!</p>`
 }
+//10
+var numUserArraForTask10=[]
+var c10=1
+function enterNumForTask10() {
+    if(numUserArraForTask10.length<5 && userInputforTask10.value<56){
+        numUserArraForTask10.push(userInputforTask10.value)
+        myDivForTask10.innerHTML+=`
+        <p>num${c10}:${userInputforTask10.value}</p>`
+        c10++
+    }
+    else{
+        alert('cant insert more then five or number bigger then 56 or smaller then 0')
+    }
+}
+var randomNumberArray=[]
+for (let j = 0; j < 5; j++) {
+    randomNumberArray.push(Math.floor(Math.random() * 56))
+}
+console.log(randomNumberArray);
+function showIfUserSiccessForTask10() {
+    for (let g = 0; g < numUserArraForTask10.length; g++) {
+        for (let h = 0; h < randomNumberArray.length; h++) {
+            if(numUserArraForTask10[g]==randomNumberArray[h]){
+                myDivForTask10.innerHTML+=`
+                <p>good!</p>`
+                return
+            }
+            
+        }
+    }
+    myDivForTask10.innerHTML+=`
+    <p>bad!</p>`
+}
