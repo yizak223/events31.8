48bd175920da7432efa015a4a654f76d3f960e34
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-09-02 20:27:46 +0300

    task9

---
+++ 9-11/main.js
@@ -0,0 +1,25 @@
+var numUserArray=[]
+
+function enterNum() {
+    if(numUserArray.length<5 && userInput.value<56){
+        numUserArray.push(userInput.value)
+        myDiv.innerHTML+=`
+        <p>num:${userInput.value}</p>`
+    }
+    else{
+        alert('cant insert more then five or number bigger then 56 or smaller then 0')
+    }
+}
+var randomNumber=Math.floor(Math.random() * 56)
+console.log(randomNumber);
+function showIfUserSiccess() {
+    for (let i = 0; i < numUserArray.length; i++) {
+        if(randomNumber==numUserArray[i]){
+            myDiv.innerHTML+=`
+            <p>good!</p>`
+            return
+        }  
+    }
+    myDiv.innerHTML+=`
+    <p>bad!</p>`
+}
